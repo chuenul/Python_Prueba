fe756ebdd62fdd3136503910f8ffd6fc752a38af
chuenul <119965127+chuenul@users.noreply.github.com>
2022-12-15 14:59:50 -0300

    Create Mayus_Minusc.py

---
+++ Mayus_Minusc.py
@@ -0,0 +1,11 @@
+proverb = 'quien a buen árbol se arrima Buena Sombra le cobija' 
+print(proverb.capitalize())
+#'Quien a buen árbol se arrima buena sombra le cobija'
+print(proverb.title())
+#'Quien A Buen Árbol Se Arrima Buena Sombra Le Cobija'
+print(proverb.upper())
+#QUIEN A BUEN ÁRBOL SE ARRIMA BUENA SOMBRA LE COBIJA􏰀
+print(proverb.lower())
+#quien a buen árbol se arrima buena sombra le cobija􏰀
+print(proverb.swapcase())
+#QUIEN A BUEN ÁRBOL SE ARRIMA bUENA sOMBRA LE COBIJA􏰀
