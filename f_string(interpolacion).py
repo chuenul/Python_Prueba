7d6d367243d2a2e3e1f284c2a2320871e29b9f36
chuenul <119965127+chuenul@users.noreply.github.com>
2022-12-18 18:37:33 -0300

    Update f_string(interpolacion).py

--- f_string(interpolacion).py
+++ f_string(interpolacion).py
@@ -3,3 +3,7 @@ age = 49
 fortune = 43_300
 print(f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones')
 #Me llamo Elon Musk, tengo 49 años y una fortuna de 43300 millones'
+
+#para imprimir llave  
+x = 10
+print(f'The variable is {{ x = {x} }}' 'The variable is { x = 10 }')
