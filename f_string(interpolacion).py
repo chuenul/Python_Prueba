2255172a1e74acf61b75f4aab81a06b1e06f6ae6
chuenul <119965127+chuenul@users.noreply.github.com>
2022-12-18 18:35:53 -0300

    Create f_string(interpolacion).py

---
+++ f_string(interpolacion).py
@@ -0,0 +1,5 @@
+name = 'Elon Musk'
+age = 49
+fortune = 43_300
+print(f'Me llamo {name}, tengo {age} años y una fortuna de {fortune} millones')
+#Me llamo Elon Musk, tengo 49 años y una fortuna de 43300 millones'
